f7a272b10aecdb23ed55b1490dc9cf196b87e716
DESKTOP-LU20QSQ\Роман <r.a.moiseenko@gmail.com>
2025-06-05 15:53:31 +0200

    Road View Model

---
+++ Assets/Scripts/Game/GamePlay/View/Roads/RoadBinder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Game.GamePlay.View.Roads
+{
+    public class RoadBinder : MonoBehaviour
+    {
+        public void Bind(RoadViewModel viewModel)
+        {
+            transform.position = new Vector3(
+                viewModel.Position.CurrentValue.x,
+                1,
+                viewModel.Position.CurrentValue.y
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/Game/GamePlay/View/Roads/RoadViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Game.State.Maps.Roads;
+using R3;
+using UnityEngine;
+
+namespace Game.GamePlay.View.Roads
+{
+    public class RoadViewModel
+    {
+        private readonly RoadEntity _roadEntity;
+        public ReadOnlyReactiveProperty<Vector2Int> Position { get; }
+
+        public RoadViewModel(
+            RoadEntity roadEntity
+            )
+        {
+            //TODO
+            _roadEntity = roadEntity;
+            
+            Position = roadEntity.Position;
+        }
+    }
+}
