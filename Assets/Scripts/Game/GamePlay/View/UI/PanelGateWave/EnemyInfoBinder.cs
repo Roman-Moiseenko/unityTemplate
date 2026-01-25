4decc65daf166ac27c366460a7ff734ac2f93c3b
DESKTOP-LU20QSQ\Роман <r.a.moiseenko@gmail.com>
2026-01-26 00:11:03 +0200

    +

--- Assets/Scripts/Game/GamePlay/View/UI/PanelGateWave/EnemyInfoBinder.cs
+++ Assets/Scripts/Game/GamePlay/View/UI/PanelGateWave/EnemyInfoBinder.cs
@@ -17,14 +17,12 @@ namespace Game.GamePlay.View.UI.PanelGateWave
         public void Bind(Sprite sprite, string nameEnemy, int countEnemy, int number, bool isBoss)
         {
             image.GetComponentInChildren<Image>().sprite = sprite;
-
-            //TODO isBoss
+            
             nameField.text = nameEnemy;
             countField.text = $"x{countEnemy}";
             bossBackground.gameObject.SetActive(isBoss);
             bossIcon.gameObject.SetActive(isBoss);
-
-                //F18C8C
+            
             transform.localPosition = new Vector3(transform.localPosition.x,
                 transform.localPosition.y + 110 * number,
                 transform.localPosition.z
